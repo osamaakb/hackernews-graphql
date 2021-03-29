0c9eed3d44efbde19f8dc491dee60318cf9aa632
osamaakb <osama98akb@gmail.com>
2021-03-29 16:13:58 +0300

    added pagination, sorting and return feed count

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,8 +1,19 @@
-async function feed (parent, args, context, info) {
-  return await context.prisma.link.findMany()
+async function feed(parent, args, context, info) {
+  const links = await context.prisma.link.findMany({
+    skip: args.skip,
+    take: args.take,
+    orderBy: args.orderBy
+  })
+
+  const count = await context.prisma.link.count()
+
+  return {
+    links,
+    count,
+  }
 }
 
-async function link (parent, args, context, info) {
+async function link(parent, args, context, info) {
   return await context.prisma.link.findUnique({ where: { id: +args.id } })
 }
 
